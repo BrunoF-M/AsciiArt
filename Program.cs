d7bf7d5dd1bf75708a44b9076c96ad86454d6a99
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-12 02:15:52 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Aperte enter para fazer ver a dona aranha: ");
+            Console.ReadLine();
+            
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+            Console.ResetColor();
+        }
+    }
+}
